45ec87ec4640ece991b9653eb906b760e4c74fb2
anton.mefodichev <mefody123@gmail.com>
2023-03-20 22:28:46 +0100

    Stage 4

--- src/main/kotlin/hyperskill/cofeemachine/CoffeeMachine.kt
+++ src/main/kotlin/hyperskill/cofeemachine/CoffeeMachine.kt
@@ -70,6 +70,103 @@ class CoffeeMachine {
         println(result)
     }
 
+    var waterLeft = 400
+    var milkLeft = 540
+    var coffeeLeft = 120
+    var cupsLeft = 9
+    var moneyLeft = 550
+
+    //Espresso
+    val waterForEspresso = 250
+    val coffeeForEspresso = 16
+    val moneyForEspresso = 4
+
+    //Latte
+    val waterForLatte = 350
+    val milkForLatte = 75
+    val coffeeForLatte = 20
+    val moneyForLatte = 7
+
+    //Cappuccino
+    val waterForCappuccino = 200
+    val milkForCappuccino = 100
+    val coffeeForCappuccino = 12
+    val moneyForCappuccino = 6
+
+
+    fun showInfo() {
+        println(
+            """
+                The coffee machine has:
+                $waterLeft ml of water
+                $milkLeft ml of milk
+                $coffeeLeft g of coffee beans
+                $cupsLeft disposable cups
+                $$moneyLeft of money
+            """.trimIndent()
+        )
+    }
+
+    fun selectProgram() {
+        println("\nWrite action (buy, fill, take): ")
+        val input = readln()
+        //"buy", "fill", "take"
+
+        when (input) {
+            "buy" -> buy()
+            "fill" -> fill()
+            "take" -> take()
+        }
+        println()
+    }
+
+    private fun buy() {
+        println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        val coffeeType = readln().toInt()
+
+        when (coffeeType) {
+            1 -> {
+                waterLeft -= waterForEspresso
+                coffeeLeft -= coffeeForEspresso
+                moneyLeft += moneyForEspresso
+                cupsLeft -= 1
+            }
+
+            2 -> {
+                waterLeft -= waterForLatte
+                milkLeft -= milkForLatte
+                coffeeLeft -= coffeeForLatte
+                moneyLeft += moneyForLatte
+                cupsLeft -= 1
+
+            }
+
+            3 -> {
+                waterLeft -= waterForCappuccino
+                milkLeft -= milkForCappuccino
+                coffeeLeft -= coffeeForCappuccino
+                moneyLeft += moneyForCappuccino
+                cupsLeft -= 1
+            }
+        }
+    }
+
+    private fun fill() {
+        println("Write how many ml of water you want to add:")
+        waterLeft += readln().toInt()
+        println("Write how many ml of milk you want to add:")
+        milkLeft += readln().toInt()
+        println("Write how many grams of coffee beans you want to add:")
+        coffeeLeft += readln().toInt()
+        println("Write how many disposable cups you want to add:")
+        cupsLeft += readln().toInt()
+    }
+
+    private fun take() {
+
+        println("I gave you \$${moneyLeft}")
+        moneyLeft = 0
+    }
 
 }
 
